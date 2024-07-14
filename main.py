429e70088ac68ec7230a2d8f2cbe2206e2a79928
Bryan Steven Valenzuela <bstevenvc@gmail.com>
2024-07-14 13:18:47 -0500

    Se agrego archivo main

---
+++ main.py
@@ -0,0 +1,12 @@
+import sys
+sys.path.append('C:\\Users\\User\\Desktop\\Backend Python\\Unidad 2\\Taller 3\\models')
+
+from models.ianimal import iAnimal
+from models.animal import Animal
+from models.animal_exotico import Animal_Exotico
+from models.huron import Huron
+
+mi_huron = Huron(nombre="Huroncito", peso=1.5, edad=2, pais_origen="Argentina", impuestos=0.1)
+
+sonido = mi_huron.hacer_sonido()
+print(sonido)
